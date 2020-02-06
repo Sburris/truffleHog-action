687c4bfcd6c291de801b2440fbb4557938fcf62f
stephen <sburrisjr@gmail.com>
2020-02-06 06:20:14 -0500

    Added json output and increased max_depth

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e # Abort script at first error
 
-args="--regex --max_depth=1" # Default trufflehog options
+args="--regex --json --max_depth=50" # Default trufflehog options
 
 if [ -n "${INPUT_SCANARGUMENTS}" ]; then
   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
